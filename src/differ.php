55f1a531ab64ed1bb86b06929b7faada8e595a88
hallernsk <haller@ngs.ru>
2020-12-01 01:13:13 +0700

    add gendiff-json(step-3)

---
+++ src/differ.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace GenDiff\DifferFiles;
+
+function differ($pathFile1, $pathFile2)
+{
+$fileJson1 = file_get_contents(__DIR__ . "./../$pathFile1"); // считываем содержимое файла 1
+$fileJson2 = file_get_contents(__DIR__ . "./../$pathFile2"); // считываем содержимое файла 2
+$data1 = json_decode($fileJson1, true);  // декодируем из json1 в ассоц. массив1 (true)
+$data2 = json_decode($fileJson2, true);  // декодируем из json2 в ассоц. массив2 (true)
+
+// теперь надо сравнивать два массива: $data1 и $data2
+ksort($data1);    // сортируем массив 1
+foreach ($data1 as $key => $value) {   // преобразуем значения логическое  true/false                        
+    if (is_bool($value)) {             // в строковое "true"/"false"
+        $data1[$key] = ($data1[$key] == 'true') ? 'true' : 'false';
+    }
+}
+
+ksort($data2);
+foreach ($data2 as $key => $value) {
+    if (is_bool($value)) {
+        $data2[$key] = ($data2[$key] == 'true') ? 'true' : 'false';
+    }
+}
+
+$resultArray = []; // итоговый массив
+
+foreach ($data1 as $key1 => $val1) {    // формируем итоговый массив (этап 1 - из массива 1)
+    if (array_key_exists($key1, $data2)) {
+        if ($data1[$key1] === $data2[$key1]) {
+                $resultArray[] = "   {$key1}: {$val1}" . PHP_EOL;     
+            } else {
+                $resultArray[] = " - {$key1}: {$val1}" . PHP_EOL;
+                $resultArray[] = " + {$key1}: {$data2[$key1]}" . PHP_EOL;
+            }
+    } else {
+            $resultArray[] = " - {$key1}: {$val1}" . PHP_EOL;
+        }
+}
+
+foreach ($data2 as $key2 => $val2) {  //    ... этап 2 - неохваченные элементы массива 2
+    if (!array_key_exists($key2, $data1)) {
+        $resultArray[] = " + {$key2}: {$val2}" . PHP_EOL;   
+    }
+}
+// оформляем результат в соотв-ии с заданием (при этом массив - в строку)
+$resultString = '{' . PHP_EOL . implode($resultArray) . '}' . PHP_EOL; 
+return $resultString;
+} 
