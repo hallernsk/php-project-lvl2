b506889c6e46fcc56c2b8b3d2375d1a1992ad442
hallernsk <haller@ngs.ru>
2020-12-08 00:53:33 +0700

    fixed differ.php

--- src/differ.php
+++ src/differ.php
@@ -9,43 +9,46 @@ function differ($pathFile1, $pathFile2)
     $data1 = readData($pathFile1);  // получаем ассоц. массив1 из файла1
     $data2 = readData($pathFile2);  // получаем ассоц. массив2 из файла2
 
-// сортируем массивы:
+    // сортируем массивы:
     ksort($data1);
-    foreach ($data1 as $key => $value) {   // преобразуем значения логическое  true/false
+    foreach ($data1 as $key => $value) {   // преобразуем логическое true/false
         if (is_bool($value)) {             // в строковое "true"/"false"
-            $data1[$key] = (boolval($data1[$key])) ? 'true' : 'false';
+            $data1[$key] = (bool)$data1[$key] ? 'true' : 'false';
         }
     }
     ksort($data2);
     foreach ($data2 as $key => $value) {
         if (is_bool($value)) {
-            $data2[$key] = (boolval($data2[$key])) ? 'true' : 'false';
+            $data2[$key] = (bool)$data2[$key] ? 'true' : 'false';
         }
     }
 
-// новый алгоритм: сначала мержим массивы в один:
+    // новый алгоритм: сначала мержим массивы в один:
     $unitedArray = array_merge($data1, $data2);
     ksort($unitedArray);
 
     $resultArray = []; // итоговый массив
-
-    foreach ($unitedArray as $key => $val) {          // формируем итоговый массив из объединенного(проходя его в цикле)
-        if (array_key_exists($key, $data1)) {         // ключ есть в М1
-            if (array_key_exists($key, $data2)) {     // ключ есть и в М2
-                if ($data1[$key] === $data2[$key]) {  // значения по этому ключу одинаковы в М1 и М2
-                    $resultArray[] = "   {$key}: {$val}" . PHP_EOL; // строка без знака
-                } else {                                            // значения не равны
-                    $resultArray[] = " - {$key}: {$data1[$key]}" . PHP_EOL; // - строка
-                    $resultArray[] = " + {$key}: {$val}" . PHP_EOL;         // + строка
-                }
-            } else {                                               // ключа нет в М2
-                $resultArray[] = " - {$key}: {$val}" . PHP_EOL;    // - строка
-            }
-        } else {                                              // ключа нет в М1
-            $resultArray[] = " + {$key}: {$val}" . PHP_EOL;   // + строка
+    // формируем итоговый массив из объединенного(проходя его в цикле):
+    foreach ($unitedArray as $key => $val) {
+        if (!array_key_exists($key, $data2)) {            // ключ есть только в М1
+            $resultArray[] = " - {$key}: {$data1[$key]}"; // - строка
+            continue;
         }
+
+        if (!array_key_exists($key, $data1)) {         // ключ есть только в М2
+            $resultArray[] = " + {$key}: {$data2[$key]}"; // + строка
+            continue;
+        }
+
+        if ($data1[$key] === $data2[$key]) {  // значения по ключу равны в М1 и М2
+            $resultArray[] = "   {$key}: {$data1[$key]}"; // строка без знака
+            continue;
+        }
+        // значения по ключу не равны:
+        $resultArray[] = " - {$key}: {$data1[$key]}";         // - строка
+        $resultArray[] = " + {$key}: {$data2[$key]}";         // + строка
     }
-// оформляем результат в соотв-ии с заданием (при этом массив - в строку)
-    $resultString = '{' . PHP_EOL . implode($resultArray) . '}' . PHP_EOL;
+    // оформляем результат в соотв-ии с заданием (при этом массив - в строку)
+    $resultString = '{' . PHP_EOL . implode(PHP_EOL, $resultArray) . PHP_EOL . '}' . PHP_EOL;
     return $resultString;
 }
