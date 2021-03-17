228fbfe44833668e17b93c51a3c48b5ea1ea7ac8
hallernsk <haller@ngs.ru>
2021-03-17 23:58:02 +0700

    fixed DifferTest.php

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -11,42 +11,42 @@ class DifferTest extends TestCase
     public function testDifferStylishJson()
     {
         $correctDiff = (file_get_contents(__DIR__ . '/fixtures/correctDiffStylish'));
-        $resultDiff = differ('tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'stylish');
+        $resultDiff = differ(__DIR__ . '/fixtures/file1.json', __DIR__ . '/fixtures/file2.json', 'stylish');
         $this->assertEquals($correctDiff, $resultDiff);
     }
 
     public function testDifferStylishYaml()
     {
         $correctDiff = (file_get_contents(__DIR__ . '/fixtures/correctDiffStylish'));
-        $resultDiff = differ('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml', 'stylish');
+        $resultDiff = differ(__DIR__ . '/fixtures/file1.yml', __DIR__ . '/fixtures/file2.yml', 'stylish');
         $this->assertEquals($correctDiff, $resultDiff);
     }
 
     public function testDifferPlainJson()
     {
         $correctDiff = (file_get_contents(__DIR__ . '/fixtures/correctDiffPlain'));
-        $resultDiff = differ('tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'plain');
+        $resultDiff = differ(__DIR__ . '/fixtures/file1.json', __DIR__ . '/fixtures/file2.json', 'plain');
         $this->assertEquals($correctDiff, $resultDiff);
     }
 
     public function testDifferPlainYaml()
     {
         $correctDiff = (file_get_contents(__DIR__ . '/fixtures/correctDiffPlain'));
-        $resultDiff = differ('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml', 'plain');
+        $resultDiff = differ(__DIR__ . '/fixtures/file1.yml', __DIR__ . '/fixtures/file2.yml', 'plain');
         $this->assertEquals($correctDiff, $resultDiff);
     }
 
     public function testDifferJsonJson()
     {
         $correctDiff = (file_get_contents(__DIR__ . '/fixtures/correctDiffJson'));
-        $resultDiff = differ('tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'json');
+        $resultDiff = differ(__DIR__ . '/fixtures/file1.json', __DIR__ . '/fixtures/file2.json', 'json');
         $this->assertEquals($correctDiff, $resultDiff);
     }
 
     public function testDifferJsonYaml()
     {
         $correctDiff = (file_get_contents(__DIR__ . '/fixtures/correctDiffJson'));
-        $resultDiff = differ('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml', 'json');
+        $resultDiff = differ(__DIR__ . '/fixtures/file1.yml', __DIR__ . '/fixtures/file2.yml', 'json');
         $this->assertEquals($correctDiff, $resultDiff);
     }
 }
