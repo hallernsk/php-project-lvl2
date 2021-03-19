a19707d17601fe968fb8d03237783751d44dddf3
hallernsk <haller@ngs.ru>
2021-03-19 23:36:51 +0700

    fixed differMain.php

--- src/differMain.php
+++ src/differMain.php
@@ -7,15 +7,13 @@ use function GenDiff\Parsers\parse;
 use function GenDiff\BuildDiffTree\buildDiffTree;
 use function GenDiff\Formatters\format;
 
-function differ($pathFile1, $pathFile2, $format)
+function differ(string $filePath1, string $filePath2, string $format): string
 {
-    [$content1, $format1] = readFile($pathFile1);    // считываем контент и формат
-    [$content2, $format2] = readFile($pathFile2);
-
-    $data1 = parse($content1, $format1);    // парсим контент(в зав-ти от формата)
+    [$content1, $format1] = readFile($filePath1);
+    [$content2, $format2] = readFile($filePath2);
+    $data1 = parse($content1, $format1);
     $data2 = parse($content2, $format2);
 
-    $diffArray = buildDiffTree($data1, $data2);      // определяем диф
-    $formaterString = format($diffArray, $format);           // форматируем диф
-    return $formaterString;
+    $diffArray = buildDiffTree($data1, $data2);
+    return format($diffArray, $format);
 }
