8cee9fe1ae37ba535497cbe0c193ba65a14ae313
hallernsk <haller@ngs.ru>
2021-03-30 21:52:02 +0700

    fixed FileReader.php

--- src/FileReader.php
+++ src/FileReader.php
@@ -8,17 +8,7 @@ function readFile(string $filePath): array
     if (!file_exists($filePath)) {
         throw new \Exception("Incorrect path to file: $filePath");
     }
-    $content = readFileContent($filePath);
-    $format = getFileFormat($filePath);
+    $content = file_get_contents($filePath);
+    $format = pathinfo($filePath, PATHINFO_EXTENSION);
     return [$content, $format];
-}
-
-function readFileContent(string $filePath): string
-{
-    return file_get_contents($filePath);
-}
-
-function getFileFormat(string $filePath): string
-{
-    return pathinfo($filePath, PATHINFO_EXTENSION);
 }
